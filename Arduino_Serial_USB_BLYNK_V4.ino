723f5a9aa2e9da225b45df95469cdd87a3ba0e6b
irwfhrjerkj <97890696+irwfhrjerkj@users.noreply.github.com>
2022-01-17 17:07:08 +0400

    Add files via upload

---
+++ Arduino_Serial_USB_BLYNK_V4.ino
@@ -0,0 +1,122 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  =>
+  =>          USB HOWTO: http://tiny.cc/BlynkUSB
+  =>
+
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT DebugSerial
+
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+//#define BLYNK_TEMPLATE_ID   "YourTemplateID"
+
+
+// You could use a spare Hardware Serial on boards that have it (like Mega)
+#include <SoftwareSerial.h>
+SoftwareSerial DebugSerial(2, 3); // RX, TX
+
+#include <BlynkSimpleStream.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "BJ-_EeNSGaf4QriezS6CxoRqhNRNQBed";
+#define m11 5
+#define m12 6
+#define m21 10
+#define m22 9
+void forward()
+{
+  digitalWrite(m11, HIGH);
+  digitalWrite(m12,LOW);
+  digitalWrite(m21,HIGH);
+  digitalWrite(m22,LOW);
+}
+void backward()
+{
+  digitalWrite(m11, LOW);
+  digitalWrite(m12,HIGH);
+  digitalWrite(m21,LOW);
+  digitalWrite(m22,HIGH);
+}
+void right()
+{
+  digitalWrite(m11, HIGH);
+  digitalWrite(m12,LOW);
+  digitalWrite(m21,LOW);
+  digitalWrite(m22,LOW);
+}
+void left()
+{
+  digitalWrite(m11, LOW);
+  digitalWrite(m12,LOW);
+  digitalWrite(m21,HIGH);
+  digitalWrite(m22,LOW);
+}
+void Stop()
+{
+  digitalWrite(m11, LOW);
+  digitalWrite(m12,LOW);
+  digitalWrite(m21,LOW);
+  digitalWrite(m22,LOW);
+}
+
+
+void setup()
+{
+  // Debug console
+  DebugSerial.begin(9600);
+
+  // Blynk will work through Serial
+  // Do not read or write this serial manually in your sketch
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+   pinMode(m11, OUTPUT);
+  pinMode(m12, OUTPUT);
+  pinMode(m21, OUTPUT);
+  pinMode(m22, OUTPUT);
+}
+BLYNK_WRITE(V1) {
+  int x = param[0].asInt();
+  int y = param[1].asInt();
+  // Do something with x and y
+/*  Serial.print("X = ");
+  Serial.print(x);
+  Serial.print("; Y = ");
+  Serial.println(y);*/
+  if(y>220)
+  forward();
+  else if(y<35)
+  backward();
+  else if(x>220)
+  right();
+  else if(x<35)
+  left();
+  else
+  Stop();
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
